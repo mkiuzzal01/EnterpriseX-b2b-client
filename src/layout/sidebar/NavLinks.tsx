9b1f152d16b28f6a4f6910571a641bbc4b1037fb
MD Khairul Islam <mkiuzzal07@gmail.com>
2025-05-19 11:58:59 +0600

    add routes for routing

--- src/layout/sidebar/NavLinks.tsx
+++ src/layout/sidebar/NavLinks.tsx
@@ -1,31 +1,172 @@
-import { BsGraphUpArrow } from "react-icons/bs";
-import { IoMdPerson, IoMdPersonAdd } from "react-icons/io";
+import {
+  FaArrowTrendUp,
+  FaList,
+  FaPaypal,
+  FaPerson,
+  FaPlus,
+  FaProductHunt,
+  FaUserGroup,
+} from "react-icons/fa6";
+import { FaUsersCog } from "react-icons/fa";
+import { IoIosNotifications } from "react-icons/io";
+import { BsChatSquareQuoteFill } from "react-icons/bs";
+import { MdFeedback } from "react-icons/md";
+import { MdOutlineBorderColor } from "react-icons/md";
+import { GrGallery } from "react-icons/gr";
+import { RiGalleryFill } from "react-icons/ri";
+import { FaFolderOpen } from "react-icons/fa";
 
 export const NavLinks = [
   {
     name: "Overview",
-    icon: <BsGraphUpArrow />,
-    children: [{ name: "Activities", icon: <BsGraphUpArrow />, route: "/" }],
+    icon: <FaArrowTrendUp />,
+    children: [{ name: "Activities", icon: <FaArrowTrendUp />, route: "/" }],
   },
   {
     name: "Profile",
-    icon: <IoMdPerson />,
+    icon: <FaPerson />,
     children: [
       {
         name: "View Profile",
-        icon: <IoMdPerson />,
+        icon: <FaPerson />,
         route: "/profile",
       },
     ],
   },
   {
     name: "Users",
-    icon: <IoMdPersonAdd />,
+    icon: <FaUsersCog />,
     children: [
       {
         name: "Create users",
-        icon: <IoMdPersonAdd />,
+        icon: <FaPerson />,
         route: "/create-user",
+      },
+      {
+        name: "All users",
+        icon: <FaUserGroup />,
+        route: "/all-users",
+      },
+    ],
+  },
+  {
+    name: "Product",
+    icon: <FaProductHunt />,
+    children: [
+      {
+        name: "Add Product",
+        icon: <FaPlus />,
+        route: "/add-product",
+      },
+      {
+        name: "All products",
+        icon: <FaList />,
+        route: "/all-users",
+      },
+      {
+        name: "Add variant",
+        icon: <FaPlus />,
+        route: "/create-variant",
+      },
+      {
+        name: "All Variant",
+        icon: <FaList />,
+        route: "/all-variant",
+      },
+      {
+        name: "Add category",
+        icon: <FaPlus />,
+        route: "/create-category",
+      },
+      {
+        name: "All category",
+        icon: <FaList />,
+        route: "/all-category",
+      },
+    ],
+  },
+  {
+    name: "Order",
+    icon: <MdOutlineBorderColor />,
+    children: [
+      {
+        name: "Add order",
+        icon: <FaPlus />,
+        route: "/create-order",
+      },
+      {
+        name: "All order",
+        icon: <FaList />,
+        route: "/all-order",
+      },
+      {
+        name: "Seller Pay",
+        icon: <FaPaypal />,
+        route: "/seller-pay",
+      },
+    ],
+  },
+  {
+    name: "Notice",
+    icon: <IoIosNotifications />,
+    children: [
+      {
+        name: "Add notice",
+        icon: <FaPlus />,
+        route: "/create-notice",
+      },
+      {
+        name: "All notice",
+        icon: <FaList />,
+        route: "/all-notice",
+      },
+    ],
+  },
+  {
+    name: "Requisition",
+    icon: <BsChatSquareQuoteFill />,
+    children: [
+      {
+        name: "Add requisition",
+        icon: <FaPlus />,
+        route: "/add-requisition",
+      },
+      {
+        name: "All requisition",
+        icon: <FaList />,
+        route: "/all-requisition",
+      },
+    ],
+  },
+  {
+    name: "Feedback",
+    icon: <MdFeedback />,
+    children: [
+      {
+        name: "Add Feedback",
+        icon: <FaPlus />,
+        route: "/add-feedback",
+      },
+      {
+        name: "All feedback",
+        icon: <FaList />,
+        route: "/all-feedback",
+      },
+    ],
+  },
+  {
+    name: "Gallery",
+    icon: <GrGallery />,
+    children: [
+      {
+        name: "All Photo",
+        icon: <RiGalleryFill />,
+        route: "/All-Photo",
+      },
+      {
+        name: "All feedback",
+        icon: <FaFolderOpen />,
+        route: "/all-folder",
       },
     ],
   },
