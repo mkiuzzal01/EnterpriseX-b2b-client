61db79bc2c0488c17b3f04621407ac9def45c827
mkiuzzal01 <mkiuzzal07@gmail.com>
2025-06-03 16:13:06 +0600

    update the category api in redux

--- src/redux/features/category/category-api.ts
+++ src/redux/features/category/category-api.ts
@@ -4,97 +4,97 @@ const categoryApi = baseApi.injectEndpoints({
   endpoints: (builder) => ({
     createSubCategory: builder.mutation({
       query: (data) => ({
-        url: "",
+        url: "/category/create-sub-category",
         method: "POST",
         body: data,
       }),
     }),
     updateSubCategory: builder.mutation({
-      query: (data) => ({
-        url: "",
+      query: (id: string) => ({
+        url: `/category/update-sub-category/${id}`,
         method: "PATCH",
-        body: data,
+        // body: data,
       }),
     }),
-    deleteSubCategory: builder.query({
-      query: () => ({
-        url: "",
+    deleteSubCategory: builder.mutation({
+      query: (id: string) => ({
+        url: `/category/delete-sub-category/${id}`,
         method: "DELETE",
       }),
     }),
     allSubCategory: builder.query({
       query: () => ({
-        url: "",
-        method: "POST",
+        url: "/category/all-sub-category",
+        method: "GET",
       }),
     }),
     singleSubCategory: builder.query({
       query: (id: string) => ({
-        url: `${id}`,
-        method: "POST",
+        url: `/category/single-sub-category${id}`,
+        method: "GET",
       }),
     }),
     createMainCategory: builder.mutation({
       query: (data) => ({
-        url: "",
+        url: "/category/create-category",
         method: "POST",
         body: data,
       }),
     }),
     updateMainCategory: builder.mutation({
-      query: (data) => ({
-        url: "",
+      query: (id: string) => ({
+        url: `/category/update-category/${id}`,
         method: "PATCH",
-        body: data,
+        // body: data,
       }),
     }),
-    deleteMainCategory: builder.query({
-      query: () => ({
-        url: "",
+    deleteMainCategory: builder.mutation({
+      query: (id: string) => ({
+        url: `/category/delete-category/${id}`,
         method: "DELETE",
       }),
     }),
     allMainCategory: builder.query({
       query: () => ({
-        url: "",
+        url: "category/all-category",
         method: "GET",
       }),
     }),
     singleMainCategory: builder.query({
-      query: () => ({
-        url: "",
+      query: (id) => ({
+        url: `category/single-category/${id}`,
         method: "GET",
       }),
     }),
     createCategory: builder.mutation({
       query: (data) => ({
-        url: "",
+        url: "/category/create-category",
         method: "POST",
         body: data,
       }),
     }),
     updateCategory: builder.mutation({
-      query: (data) => ({
-        url: "",
-        method: "POST",
-        body: data,
+      query: (id: string) => ({
+        url: `/category/update-category/${id}`,
+        method: "PATCH",
+        // body:
       }),
     }),
-    deleteCategory: builder.query({
-      query: () => ({
-        url: "",
+    deleteCategory: builder.mutation({
+      query: (id: string) => ({
+        url: `/category/delete-category/${id}`,
         method: "DELETE",
       }),
     }),
     allCategory: builder.query({
       query: () => ({
-        url: "",
+        url: "/category/all-category",
         method: "GET",
       }),
     }),
     singleCategory: builder.query({
-      query: () => ({
-        url: "",
+      query: (id: string) => ({
+        url: `/category/single-category/${id}`,
         method: "GET",
       }),
     }),
@@ -105,15 +105,15 @@ export const {
   useAllSubCategoryQuery,
   useSingleSubCategoryQuery,
   useUpdateSubCategoryMutation,
-  useDeleteSubCategoryQuery,
+  useDeleteSubCategoryMutation,
 
   useAllMainCategoryQuery,
   useSingleMainCategoryQuery,
-  useDeleteMainCategoryQuery,
+  useDeleteMainCategoryMutation,
   useUpdateMainCategoryMutation,
 
   useAllCategoryQuery,
   useSingleCategoryQuery,
-  useDeleteCategoryQuery,
+  useDeleteCategoryMutation,
   useUpdateCategoryMutation,
 } = categoryApi;
